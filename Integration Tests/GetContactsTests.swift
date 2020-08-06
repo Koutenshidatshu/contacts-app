7fc50c033318280db8b9f055aa0caf6de5fae1ea
yonathan <koutenshidatshu_555@yahoo.co.id>
2020-08-06 18:09:43 +0700

    Create integration test getContacts and allow ATS in info.plist

---
+++ Integration Tests/GetContactsTests.swift
@@ -0,0 +1,41 @@
+//
+//  GetContactsTests.swift
+//  contacts-APPTests
+//
+//  Created by Yonathan Wijaya on 06/08/20.
+//  Copyright © 2020 Yonathan Wijaya. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+@testable import contacts_APP
+
+class GetContactsTests: XCTestCase {
+    func testGetDatas() {
+        let jsonString = createRequestString()
+        let hasID = jsonString.contains("id")
+        XCTAssertTrue(hasID)
+        
+        let hasFirstName = jsonString.contains("first_name")
+        XCTAssertTrue(hasFirstName)
+
+        let hasLastName = jsonString.contains("last_name")
+        XCTAssertTrue(hasLastName)
+        
+        let hasProfilePic = jsonString.contains("profile_pic")
+        XCTAssertTrue(hasProfilePic)
+        
+        let hasFavorite = jsonString.contains("favorite")
+        XCTAssertTrue(hasFavorite)
+        
+        let hasUrl = jsonString.contains("url")
+        XCTAssertTrue(hasUrl)
+        
+        
+    }
+    
+    private func createRequestString() -> String {
+        let data = GetContacts().get().firstEmit()!
+        return String(data: data, encoding: .utf8)!
+    }
+}
---
+++ contacts-APP/Adapter/GetContacts.swift
@@ -0,0 +1,38 @@
+//
+//  GetContacts.swift
+//  contacts-APP
+//
+//  Created by Yonathan Wijaya on 06/08/20.
+//  Copyright © 2020 Yonathan Wijaya. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct GetContacts {
+    func get() -> Observable<Data> {
+        let session = URLSession.shared
+        let data = session.rx.response(request: requestUrl())
+            .map { (_, data) in
+                return data
+        }.retry(2)
+        return handleApiRequestFailed(data)
+    }
+    
+    private func requestUrl() -> URLRequest {
+        let url = URL(string: ContactsApi().path)!
+        let urlRequest = URLRequest(url: url)
+        return urlRequest
+    }
+    
+    private func handleApiRequestFailed(_ observable: Observable<Data>) -> Observable<Data> {
+        return observable.catchError { error -> Observable<Data> in
+            if case let .httpRequestFailed(_, data)? = error as? RxCocoaURLError {
+                return Observable.from(optional: data)
+            } else {
+                return .error(error)
+            }
+        }
+    }
+}
